7eb7da1b723e8e246ad843b87cda0bcd64c3d4ca
mp_s <mp-s@users.noreply.github.com>
2018-07-19 12:08:06 +0800

    chapter 3b

--- hello.py
+++ hello.py
@@ -10,6 +10,15 @@ Bootstrap = Bootstrap(app)
 
 my_dict = {'key': 'test'}
 
+# before '/'
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 @app.route('/')
 def index():
     return render_template('index.html', mydict=my_dict)
