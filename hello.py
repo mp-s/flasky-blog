886a67dbf89af52d82ba9d3bc30ec5e002de96fb
mp_s <mp-s@users.noreply.github.com>
2018-07-20 17:13:46 +0800

    chapter 4 form 1

--- hello.py
+++ hello.py
@@ -1,17 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_script import Manager, Server
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hardtoguess12345HARD'
 manager = Manager(app)
 manager.add_command("runserver", Server(use_debugger=True))
 Bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 my_dict = {'key': 'test'}
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
 
 # before '/'
 @app.errorhandler(404)
@@ -24,7 +33,8 @@ def internal_server_error(e):
 
 @app.route('/')
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm(request.form)
+    return render_template('index.html', current_time=datetime.utcnow(), form=form)
 
 
 @app.route('/user/<name>')
