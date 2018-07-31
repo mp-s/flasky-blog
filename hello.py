721e4672cd54b0dd69ca4902ca015df3e92e560e
mp_s <mp-s@users.noreply.github.com>
2018-07-31 17:42:23 +0800

    chapter 6b async_email

--- hello.py
+++ hello.py
@@ -10,6 +10,7 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_mail import Mail, Message
 
 from datetime import datetime
+from threading import Thread
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -41,12 +42,18 @@ manager.add_command('db', MigrateCommand)
 
 mail = Mail(app)
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send()
+
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, 
                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
