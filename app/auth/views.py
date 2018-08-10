fd4a0c35f7d910076a0f4faa47f10ca0bfed8c9d
mp_s <mp-s@users.noreply.github.com>
2018-08-10 15:20:19 +0800

    8.4.6 fix

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, login_required
+from flask_login import login_user, logout_user, login_required
 from . import auth
 from ..models import User
 from .forms import LoginForm
@@ -11,7 +11,10 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and  user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
+            next = request.args.get('next')
+            if next is None or not next.startswith('/'):
+                next = url_for('main.index')
+            return redirect(next)
         flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
 
