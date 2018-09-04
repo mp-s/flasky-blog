25f4ded814b626ee4df81d2e47178bd769a32d7e
mp_s <mp-s@users.noreply.github.com>
2018-09-04 14:50:12 +0800

    chapter 10.2 fix

--- app/auth/views.py
+++ app/auth/views.py
@@ -60,7 +60,9 @@ def before_request():
     if current_user.is_authenticated:
         current_user.ping()
         if not current_user.confirmed \
-                and request.endpoint[:5] != 'auth.':
+                and request.endpoint \
+                and request.blueprint != 'auth' \
+                and request.endpoint != 'static':
             return redirect(url_for('auth.unconfirmed'))
 
 @auth.route('/unconfirmed')
