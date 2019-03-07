45719c9a9e725d7a859a3490250206d4413df2d3
mp_s <mp-s@users.noreply.github.com>
2019-03-07 20:40:31 +0800

    10.3.1 用户级别的资料编辑器

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash, abort
+from flask_login import login_required, current_user
 
 from . import main
+from .forms import EditProfileForm
+from .. import db
 from ..models import User
 
 @main.route('/')
@@ -13,3 +16,20 @@ def user(username):
     if user is None:
         abort(404)
     return render_template('user.html', user=user)
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+
+    return render_template('edit_profile.html', form=form)
