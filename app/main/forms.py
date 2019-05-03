ef7afff64fca4fa98a7bc2611f39ba09944ce2eb
mp_s <mp-s@users.noreply.github.com>
2019-05-03 16:00:46 +0800

    11.1

--- app/main/forms.py
+++ app/main/forms.py
@@ -43,3 +43,7 @@ class EditProfileAdminForm(FlaskForm):
         if field.data != self.user.username and \
                 User.query.filter_by(username=field.data).first():
             raise ValidationError('username already in use.')
+
+class PostForm(FlaskForm):
+    body = TextAreaField('你现在在想什么', validators=[Required()])
+    submit = SubmitField('Submit')
--- app/models.py
+++ app/models.py
@@ -88,6 +88,8 @@ class User(UserMixin, db.Model):
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
     # avatar
     avatar_hash = db.Column(db.String(32))
+    # posts
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -201,6 +203,13 @@ class AnonymousUser(AnonymousUserMixin):
     
     def is_administrator(self):
         return False
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DataTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 login_manager.anonymous_user = AnonymousUser
 
