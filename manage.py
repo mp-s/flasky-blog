949188bc7688bfe917881d1bf9143be76e0ecf65
mp_s <mp-s@users.noreply.github.com>
2020-01-15 22:05:20 +0800

    修复错误, 前进到17.2

--- manage.py
+++ manage.py
@@ -12,7 +12,7 @@ if os.environ.get('FLASK_COVERAGE'):
 
 from app import create_app, db
 from app.models import User, Role, Permission, Post, Follow, Comment
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -56,31 +56,32 @@ def test(coverage=False):
         COV.erase()
 
 
-@app.cli.command
+@app.cli.command()
 @click.option('--length',
               default=25,
               help='Number of functions to include in the profiler report.')
 @click.option('--profile-dir',
               default=None,
               help='Directory where profiler data files are saved.')
-def profile(length=25, profile_dir=None):
+def profile(length, profile_dir):
     ''' Start the application under the code profiler. '''
     from werkzeug.contrib.profiler import ProfilerMiddleware
     app.wsgi_app = ProfilerMiddleware(app.wsgi_app,
                                       restrictions=[length],
                                       profile_dir=profile_dir)
-    app.run(debug=False)
+    app.run()
 
 
-@app.cli.command
+@app.cli.command()
 def deploy():
-    from flask_migrate import upgrade
-    from app.models import Role, User
+    """ Run deployment tasks. """
 
     # 把数据库迁移到最新修订版本
     upgrade()
+
     # 创建用户角色
     Role.insert_roles()
+
     # 所有用户关注此用户
     User.add_self_follows()
 
